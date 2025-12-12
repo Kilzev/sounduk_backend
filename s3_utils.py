5c7b59e1d45d23ba05b0994c0edc95de2d2c9609
Kilzev <knyzeiv@gmail.com>
2025-12-12 02:54:26 -0500

    Add certifi for S3 SSL verification

--- s3_utils.py
+++ s3_utils.py
@@ -2,6 +2,7 @@
 import boto3
 import os
 from dotenv import load_dotenv
+import certifi
 
 load_dotenv()
 
@@ -11,7 +12,8 @@ def get_s3_client():
         endpoint_url=os.getenv("S3_ENDPOINT_URL"),
         aws_access_key_id=os.getenv("S3_ACCESS_KEY_ID"),
         aws_secret_access_key=os.getenv("S3_SECRET_ACCESS_KEY"),
-        region_name=os.getenv("S3_REGION_NAME")
+        region_name=os.getenv("S3_REGION_NAME"),
+        verify=certifi.where()
     )
 
 S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
